3550d2ba8eaeefea7a6b8650a7b0de7858a376f5
Gene Bogdanovich <genebogdanovich@gmail.com>
2020-12-22 11:42:42 +0300

    Simplify forecast response models.

--- Misty/Models/CurrentWeatherForecastResponse.swift
+++ Misty/Models/CurrentWeatherForecastResponse.swift
@@ -1,84 +1,53 @@
-// This file was generated from JSON Schema using quicktype, do not modify it directly.
-// To parse the JSON, add this file to your project and do:
-//
-//   let currentWeatherForecastResponse = try? newJSONDecoder().decode(CurrentWeatherForecastResponse.self, from: jsonData)
-
 import Foundation
 
-// MARK: - CurrentWeatherForecastResponse
-struct CurrentWeatherForecastResponse: Codable {
+struct CurrentWeatherForecastResponse: Decodable {
     let coord: Coord
-    let weather: [Weather]
-    let base: String
     let main: Main
-    let visibility: Double
-    let wind: Wind
-    let clouds: Clouds
-    let dt: Double
-    let sys: Sys
-    let timezone: Double
-    let id: Double
     let name: String
-    let cod: Double
+    let weather: [Weather]
+    let sys: Sys
+    let wind: Wind
+    let dt: Double
+    
+    struct Main: Codable {
+        let temperature: Double
+        let humidity: Double
+        let maxTemperature: Double
+        let minTemperature: Double
+        let feelsLike: Double
+        let pressure: Double
+        
+        enum CodingKeys: String, CodingKey {
+            case temperature = "temp"
+            case humidity
+            case maxTemperature = "temp_max"
+            case minTemperature = "temp_min"
+            case feelsLike = "feels_like"
+            case pressure
+        }
+    }
+    
+    struct Coord: Codable {
+        let lon: Double
+        let lat: Double
+    }
+}
+
+struct Weather: Decodable {
+    let id: Int
+    let main: String
+    let weatherDescription: String
+    let icon: String
 
     enum CodingKeys: String, CodingKey {
-        case coord = "coord"
-        case weather = "weather"
-        case base = "base"
-        case main = "main"
-        case visibility = "visibility"
-        case wind = "wind"
-        case clouds = "clouds"
-        case dt = "dt"
-        case sys = "sys"
-        case timezone = "timezone"
         case id = "id"
-        case name = "name"
-        case cod = "cod"
+        case main = "main"
+        case weatherDescription = "description"
+        case icon = "icon"
     }
 }
 
-// MARK: - Clouds
-struct Clouds: Codable {
-    let all: Double
-
-    enum CodingKeys: String, CodingKey {
-        case all = "all"
-    }
-}
-
-// MARK: - Coord
-struct Coord: Codable {
-    let lon: Double
-    let lat: Double
-
-    enum CodingKeys: String, CodingKey {
-        case lon = "lon"
-        case lat = "lat"
-    }
-}
-
-// MARK: - Main
-struct Main: Codable {
-    let temp: Double
-    let feelsLike: Double
-    let tempMin: Double
-    let tempMax: Double
-    let pressure: Double
-    let humidity: Double
-
-    enum CodingKeys: String, CodingKey {
-        case temp = "temp"
-        case feelsLike = "feels_like"
-        case tempMin = "temp_min"
-        case tempMax = "temp_max"
-        case pressure = "pressure"
-        case humidity = "humidity"
-    }
-}
-
-// MARK: - Sys
-struct Sys: Codable {
+struct Sys: Decodable {
     let type: Double
     let id: Double
     let country: String
@@ -94,23 +63,7 @@ struct Sys: Codable {
     }
 }
 
-// MARK: - Weather
-struct Weather: Codable {
-    let id: Double
-    let main: String
-    let weatherDescription: String
-    let icon: String
-
-    enum CodingKeys: String, CodingKey {
-        case id = "id"
-        case main = "main"
-        case weatherDescription = "description"
-        case icon = "icon"
-    }
-}
-
-// MARK: - Wind
-struct Wind: Codable {
+struct Wind: Decodable {
     let speed: Double
     let deg: Double
 
@@ -119,4 +72,3 @@ struct Wind: Codable {
         case deg = "deg"
     }
 }
-
